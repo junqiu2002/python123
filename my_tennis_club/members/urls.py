01aecc24e1531975013f836d25c8eead24bcbdc6
junqiu2002 <jun.gary.qiu@gmail.com>
2024-09-18 10:35:39 -0700

    3.10 different with 3.8? so  update urls.py

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # for python 3.8
+    # path('members/', views.members, name='members'),
+
+    # doe python 3.10
     path('members/', views.members, name='members'),
 ]
 
